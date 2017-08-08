2d89a81586a9dff82671a387cd1bcf3dff43d3be
arthurxiang <arthur.s.xiang@gmail.com>
2017-08-08 19:40:35 +0800

    nothing changed

--- src/ORBextractor.cc
+++ src/ORBextractor.cc
@@ -14,7 +14,7 @@
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
-*
+*FVSLAM
 * You should have received a copy of the GNU General Public License
 * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
 */
